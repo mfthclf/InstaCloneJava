0f1bbb0e8f21e485b598ad80200d27ee33184097
Fatih Culfa <mfthclf@gmail.com>
2024-07-15 12:48:13 +0300

    User registration created

--- app/src/main/java/com/mfthc/instaclonejava/UserFragment.java
+++ app/src/main/java/com/mfthc/instaclonejava/UserFragment.java
@@ -5,11 +5,17 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.mfthc.instaclonejava.databinding.FragmentUserBinding;
 
 
@@ -17,6 +23,7 @@ public class UserFragment extends Fragment {
 
 
     FragmentUserBinding binding;
+    FirebaseAuth auth;
 
     public UserFragment() {
         // Required empty public constructor
@@ -26,6 +33,7 @@ public class UserFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        auth = FirebaseAuth.getInstance();
 
 
     }
@@ -41,6 +49,35 @@ public class UserFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        binding.signUpButton.setOnClickListener(v -> SignUp(view));
+        binding.signInButton.setOnClickListener(v -> SignIn(view));
+
+        FirebaseUser user = auth.getCurrentUser();
+        if(user!=null){
+            NavDirections action = UserFragmentDirections.actionUserFragmentToFeedFragment();
+            Navigation.findNavController(requireView()).navigate(action);
+        }
+    }
+
+    public void SignUp(View view) {
+
+        String email = binding.emailText.getText().toString();
+        String password = binding.passwordText.getText().toString();
+        if (!email.isEmpty()&&!password.isEmpty()) {
+            auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    NavDirections action = UserFragmentDirections.actionUserFragmentToFeedFragment();
+                    Navigation.findNavController(requireView()).navigate(action);
+                }
+            }).addOnFailureListener(e -> {
+                Toast.makeText(requireContext(), e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+            });
+        }
+
+
+    }
+    public void SignIn(View view){
+
     }
 
     @Override
