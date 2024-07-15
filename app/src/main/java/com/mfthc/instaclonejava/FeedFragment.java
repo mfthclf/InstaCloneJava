8b3eb372c8790a893295ac059a1a4212592d836e
Fatih Culfa <mfthclf@gmail.com>
2024-07-15 11:56:04 +0300

    Pop-up menu is completed

--- app/src/main/java/com/mfthc/instaclonejava/FeedFragment.java
+++ app/src/main/java/com/mfthc/instaclonejava/FeedFragment.java
@@ -2,22 +2,31 @@ package com.mfthc.instaclonejava;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavDirections;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.PopupMenu;
+
+import com.mfthc.instaclonejava.databinding.FragmentFeedBinding;
 
 
-public class FeedFragment extends Fragment {
+public class FeedFragment extends Fragment implements PopupMenu.OnMenuItemClickListener {
 
+    FragmentFeedBinding binding;
+    PopupMenu menu;
 
 
     public FeedFragment() {
         // Required empty public constructor
     }
-
-
 
 
     @Override
@@ -29,7 +38,45 @@ public class FeedFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_feed, container, false);
+        binding = FragmentFeedBinding.inflate(inflater, container, false);
+        return binding.getRoot();
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        binding.floatingActionButton.setOnClickListener(v -> ShowPopupMenu(view));
+        menu = new PopupMenu(requireContext(), binding.floatingActionButton);
+        MenuInflater inflater = menu.getMenuInflater();
+        inflater.inflate(R.menu.popup_menu, menu.getMenu());
+        menu.setOnMenuItemClickListener(this);
+
+    }
+
+    public void ShowPopupMenu(View view) {
+
+        menu.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+
+        if (item.getItemId() == R.id.newPost) {
+            NavDirections action = FeedFragmentDirections.actionFeedFragmentToUploadFragment();
+            Navigation.findNavController(requireView()).navigate(action);
+        } else {
+            //Log-out
+            NavDirections action = FeedFragmentDirections.actionFeedFragmentToUserFragment();
+            Navigation.findNavController(requireView()).navigate(action);
+        }
+        return true;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+
+
 }
--- app/src/main/java/com/mfthc/instaclonejava/UserFragment.java
+++ app/src/main/java/com/mfthc/instaclonejava/UserFragment.java
@@ -2,6 +2,8 @@ package com.mfthc.instaclonejava;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -34,5 +36,16 @@ public class UserFragment extends Fragment {
         // Inflate the layout for this fragment
         binding = FragmentUserBinding.inflate(inflater, container, false);
         return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
     }
 }
